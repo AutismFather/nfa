d9287623ba51a52f6bf3c946c5d2533031f02133
Stuart Duncan <github@tycamtech.com>
2013-05-22 22:50:39 -0400

    Added - Users migration

--- bundles/admin/start.php
+++ bundles/admin/start.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Stuart
- * Date: 22/05/13
- * Time: 9:40 AM
- * To change this template use File | Settings | File Templates.
- */
 Autoloader::map(array(
     'Admin_Base_Controller' => Bundle::path('admin').'controllers/base.php',
 ));
