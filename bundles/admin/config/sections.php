b8eafcd6866305cb9a5c98bb04bfc4a1fe8f0cef
Stuart Duncan <github@tycamtech.com>
2013-06-13 23:06:07 -0400

    sections config to list all links in main menu

---
+++ bundles/admin/config/sections.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    'dashboard' => array(
+        'name' => 'dashboard',
+        'url' => '',
+        'image' => '',
+        'current' => false
+    ),
+    'products' => array(
+        'name' => 'products',
+        'url' => '',
+        'image' => '',
+        'current' => false
+    )
+)
+?>
