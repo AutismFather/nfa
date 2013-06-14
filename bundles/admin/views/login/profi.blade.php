296ff568f55351328323fbbd67fbdc16adeb9113
Stuart Duncan <github@tycamtech.com>
2013-06-13 23:04:02 -0400

    Updated form information

--- bundles/admin/views/login/profi.blade.php
+++ bundles/admin/views/login/profi.blade.php
@@ -13,18 +13,18 @@
 		<div id="login">
 			<p class="tip">You just need to hit the button and you're in!</p>
 			<p class="error">This is when something is wrong!</p>
-			<form id="form1" name="form1" method="post" action="">
+			<form id="form1" name="form1" method="post" action="{{ URL::to_action('admin@login') }}">
 				<p>
 					<label><strong>Username</strong>
-						<input type="text" name="textfield" class="inputText" id="textfield" />
+						<input type="text" name="username" class="inputText" id="username" />
 					</label>
 				</p>
 				<p>
 					<label><strong>Password</strong>
-						<input type="password" name="textfield2" class="inputText" id="textfield2" />
+						<input type="password" name="password" class="inputText" id="password" />
 					</label>
 				</p>
-				<a class="black_button" href="dashboard.html"><span>Authentification</span></a>
+                <input type='submit' value='Authentication' class='black_button'/>
 				<label>
 					<input type="checkbox" name="checkbox" id="checkbox" />
 					Remember me</label>
